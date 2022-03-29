34cb986e7373b25d29355686b6a96759e7660641
JoelEarps <joeldearps@gmail.com>
2022-03-29 09:54:35 +0100

    Linked List and Memory Manegement

--- Linked Lists/test.cpp
+++ Linked Lists/test.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 
 using namespace std;
+
+//Node is a standard reference to an element in a linked list
+
+class Node {
+    public: 
+    int Value;
+    Node* Next;
+};
+
+void printList(Node* n) {
+    //while node is not null print the current value and then move to next node
+    while (n != NULL){
+        cout << n->Value << endl;
+        n = n->Next;
+    }
+}
+
 int main () {
-    cout << "Hello World" << endl;
+
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    head->Value = 1;
+    head->Next = second;
+
+    // the next value of the current node is the next node class pointer in the linked list
+    second->Value = 2;
+    second->Next = third;
+
+    third->Value = 3;
+    third->Next = NULL;
+
+    printList(head);
+
+    //delete memory used
+    delete head;
+    delete second;
+    delete third;
+
+    
 }
